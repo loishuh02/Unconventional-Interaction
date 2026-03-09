57cc72e7c12d8311d4cda5fe4ab43bfa61a16399
loishuh02 <loishuh02@gmail.com>
2026-03-09 13:43:31 -0500

    Initial setsup

---
+++ sketch.js
@@ -0,0 +1,49 @@
+//template code
+let video;
+let handPose;
+let hands = [];
+let painting;
+let px = 0;
+let py = 0;
+
+function preload() {
+  handPose = ml5.handPose({flipped: true});
+}
+
+function mousePressed() {
+    console.log(hands);
+}
+
+function gotHands(results) {
+  hands = results;
+}
+
+function setup() {
+    createCanvas(640, 480);
+    painting = createGraphics(640, 480);
+    //painting.background(255);
+    video = createCapture(VIDEO, {flipped: true });
+    video.hide();
+    handPose.detectStart(video, gotHands);
+}
+
+function draw() {
+    image(video, 0, 0);
+    if (hands.length > 0) {
+        let hand = hands[0];
+        let index = hand.index_finger_tip;
+        let thumb = hand.thumb_tip;
+        let x = (index.x + thumb.x) / 2;
+        let y = (index.y + thumb.y) / 2;
+      
+        let d = dist(index.x, index.y, thumb.x, thumb.y);
+        if (d < 20) {
+            painting.stroke(255, 255, 0);
+            painting.strokeWeight(8);
+            painting.line(px, py, x, y);
+        }
+        px = x;
+        py = y;
+    }
+    image(painting,0,0);
+}
